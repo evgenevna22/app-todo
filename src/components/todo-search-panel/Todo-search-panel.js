a24d645643cb0bbf8f8f09910e46402398b86d39
Tatiana <tanya.evgenevna@gmail.com>
2019-03-21 20:24:14 +0300

    разделение элементов на компоненты

---
+++ src/components/todo-search-panel/Todo-search-panel.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const SearchPanel = () => {
+
+    const searchText = 'type here to search';
+    return <input
+        placeholder={searchText} />;
+};
+
+export default SearchPanel;
